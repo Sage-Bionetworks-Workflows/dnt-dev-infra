a30cfb0fea1707bb15fed8e3a62fde81ac129e9e
Brad Macdonald <52762200+BWMac@users.noreply.github.com>
2023-02-06 15:56:25 -0700

    Update utils/airflow-port-forward.sh
    Co-authored-by: Thomas Yu <thomas.yu@sagebase.org>

--- utils/airflow-port-forward.sh
+++ utils/airflow-port-forward.sh
@@ -8,10 +8,10 @@ if [[ -z $profile ]]; then
   echo "this script requires an AWS profile name as the first parameter"
 else
   set -ex
-  stack=$(aws cloudformation describe-stacks --stack-name airflow-ec2)
+  stack=$(aws --profile $profile cloudformation describe-stacks --stack-name airflow-ec2)
   target=$(echo "$stack" | jq -r '.Stacks[0].Outputs | .[] | select(.OutputKey == "Ec2InstanceId").OutputValue')
   echo $target
-  aws ssm start-session --profile $profile \
+  aws --profile $profile ssm start-session \
                       --target "${target}" \
                       --document-name AWS-StartPortForwardingSession \
                       --parameters '{"portNumber":["8080"],"localPortNumber":["8080"]}'
